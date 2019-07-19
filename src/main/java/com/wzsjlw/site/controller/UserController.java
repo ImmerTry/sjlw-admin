8ebccd5e81d846003e1d6fcc09f2b22fad8922be
LIULEI <1136715789@qq.com>
2019-07-19 18:14:57 +0800

    modified code

--- src/main/java/com/wzsjlw/site/controller/UserController.java
+++ src/main/java/com/wzsjlw/site/controller/UserController.java
@@ -5,6 +5,7 @@ import com.wzsjlw.site.service.UserService;
 import com.wzsjlw.site.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,8 +21,8 @@ public class UserController {
     UserService userService;
 
 
-    @PostMapping("/login")
-    public Result login(User user)
+    @PostMapping("/user/login")
+    public Result login(@RequestBody User user)
     {
         User login = userService.login(user);
         if (login != null) {
